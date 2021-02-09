5d2cf0b2cd18945b4ace3141e59928e760e6925f
Yevhenii Kalashnyk <35871920+ykalashn@users.noreply.github.com>
2021-02-09 17:35:58 +0200

    added analytics scripts

--- js/custom.js
+++ js/custom.js
@@ -9,11 +9,3 @@ $(function(){
 $(document).ready(function(){
 $( "#footer-placeholder" ).load( "../footer.html" );
 });
-
-// Google Analytics
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'UA-178238208-1');
