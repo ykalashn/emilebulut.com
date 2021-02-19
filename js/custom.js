e179318e8e08d9fb4c79b87c8275be4d1c643cb6
Yevhenii Kalashnyk <35871920+ykalashn@users.noreply.github.com>
2021-02-19 14:53:06 +0200

    Added newsletter, updated design, new layout for blog, handlebars

--- js/custom.js
+++ js/custom.js
@@ -4,6 +4,12 @@ $(function(){
     $("#nav-placeholder").load("https://emilebulut.com/nav.html");
 });
 
+//Newsletter
+
+$(document).ready(function(){
+    $( "#newsletter" ).load( "https://emilebulut.com/newsletter.html" );
+    });
+
 // Footer
 
 $(document).ready(function(){
