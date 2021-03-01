e3108c4f1de0ad9c4b87e0299b85a6bf412dcee2
Yevhenii Kalashnyk <35871920+ykalashn@users.noreply.github.com>
2021-03-01 23:11:53 +0200

    updated blog main page

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,17 @@
 // Navigation
 
 $(function(){
-    $("#nav-placeholder").load("https://emilebulut.com/nav.html");
+    $("#nav-placeholder").load("../../nav.html");
 });
 
 //Newsletter
 
 $(document).ready(function(){
-    $( "#newsletter" ).load( "https://emilebulut.com/newsletter.html" );
+    $( "#newsletter" ).load( "../../newsletter.html" );
     });
 
 // Footer
 
 $(document).ready(function(){
-$( "#footer-placeholder" ).load( "https://emilebulut.com/footer.html" );
+$( "#footer-placeholder" ).load( "../../footer.html" );
 });
